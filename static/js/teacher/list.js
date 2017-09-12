f93ed1c30f192404e259ea4233f717fc6149be99
hsw <hsw@hsw.com>
2017-09-12 21:06:57 +0800

    查看部分功能完成

--- static/js/teacher/list.js
+++ static/js/teacher/list.js
@@ -1,6 +1,7 @@
-define(["jquery","template"],function($,template){
+define(["jquery","template","bootstrap"],function($,template){
   $(function(){
 
+    //获取讲师列表
     $.ajax({
       url:"/api/teacher",
       success:function(data){
@@ -11,7 +12,21 @@ define(["jquery","template"],function($,template){
       }
     })
 
+    //点击显示模态框
+    $("#teacher_list_tbody").on("click",".search-btn",function(){
 
+      var id = $(this).parent().data("id");
+      $.ajax({
+        url:"/api/teacher/view",
+        data:{
+          tc_id:id
+        },
+        success:function(data){
+          console.log(data);
+          $("#teacherModal").html(template("modal_tpl",data.result)).modal("show");
+        }
+      });
+    })
 
 
   })
